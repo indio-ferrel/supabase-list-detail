c8cadb7218396419ee64653824b5ac4a5ed64025
Indio Ferrel <mferrel1911@gmail.com>
2022-05-12 15:25:31 -0700

    Implement function renderCerealDetail.

--- render-utils.js
+++ render-utils.js
@@ -12,3 +12,20 @@ export function renderCereal(cereal) {
     cerealDiv.append(a);
     return cerealDiv;
 }
+
+export function renderCerealDetail(cereal) {
+    const div = document.createElement('div');
+    div.classList.add('cereal-detail');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = cereal.name;
+
+    const img = document.createElement('img');
+    img.src = cereal.image;
+
+    const p = docuemnt.createElement('p');
+    p.textContent = `${cereal.name} has been assigned a health rating of ${cereal.health} and a deliciousness rating of ${cereal.deliciousness} by an independent ratings board.`;
+
+    div.append(h1, img, p);
+    return div;
+}
